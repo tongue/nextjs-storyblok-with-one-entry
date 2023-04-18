a47a46c56f7da795530a0dccf413ccd65f8c6b0f
Johan Bergström <johan.bergstrom@isotop.se>
2023-04-18 15:03:25 +0200

    first

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+import Unused1 from "../components/Unused1";
+import Unused2 from "../components/Unused2";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+  unsused1: Unused1,
+  unsused2: Unused2,
+};
+
+storyblokInit({
+  accessToken: "X7CoD455aCxxE2pmrj4DQAtt",
+  use: [apiPlugin],
+  components,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
